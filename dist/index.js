89f6341501422995edc7cee59d17a508ea74430c
Louis Borres <louisrborres@gmail.com>
2024-11-05 21:53:38 +0800

    build: output cjs and esm and ignore dist

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Greet = Greet;
-function Greet(input) {
-    return "Hello ".concat(input !== null && input !== void 0 ? input : "World!");
-}
