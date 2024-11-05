fe061c822acf19fb979ad63b990c3185a3e085bd
Louis Borres <louisrborres@gmail.com>
2024-11-05 22:01:41 +0800

    feat: add `Ping()` function

---
+++ README.md
@@ -0,0 +1,1 @@
+# Practicing Publishing Packages
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 export function Greet(input: string): string {
   return `Hello ${input ?? "World!"}`
 }
+
+export function Ping(): string {
+  return 'Pong'
+}
